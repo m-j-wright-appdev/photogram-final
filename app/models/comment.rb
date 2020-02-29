f37451eda3e8fe43d8f4d5f5e018421ea46c04d1
m-j-wright <michael.wright@chicagobooth.edu>
2020-02-29 03:21:47 +0000

    committ1

---
+++ app/models/comment.rb
@@ -0,0 +1,19 @@
+# == Schema Information
+#
+# Table name: comments
+#
+#  id         :integer          not null, primary key
+#  body       :text
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  author_id  :integer
+#  photo_id   :integer
+#
+class Comment < ApplicationRecord
+
+validates :body, :presence => true
+
+belongs_to :photo
+belongs_to :author, :class_name => "User"
+
+end
